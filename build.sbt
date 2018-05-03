ef388ddbfa9243a68f56f479ed4fdc8f0607acf4
Timothy Klim <klimtimothy@gmail.com>
2018-05-03 20:41:17 +0300

    leader tx

--- build.sbt
+++ build.sbt
@@ -1,12 +1,16 @@
-name := "etcd3-ring"
+name := "etcd3-sharding-ring"
 
 scalaVersion := "2.12.6"
 
+val akkaVersion = "2.5.12"
+
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.5.12",
-  "com.typesafe.akka" %% "akka-stream" % "2.5.12",
-  "com.typesafe.akka" %% "akka-slf4j" % "2.5.12",
-  "com.typesafe.akka" %% "akka-stream-kafka" % "0.20",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
   "com.coreos" % "jetcd-core" % "0.0.2",
-  "ch.qos.logback" % "logback-classic" % "1.2.3"
+  "com.github.pureconfig" %% "pureconfig" % "0.9.1",
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+  "com.typesafe.akka" %% "akka-stream-kafka" % "0.20",
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0",
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,18 @@
-import akka.Done
 import akka.actor._
+import akka.Done
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import com.coreos.jetcd._
 import com.coreos.jetcd.data._
+import com.coreos.jetcd.op._
 import com.coreos.jetcd.options._
-import com.coreos.jetcd.Watch._
 import com.coreos.jetcd.watch._
-import scala.collection.JavaConverters._
-import scala.concurrent.{Future, blocking, Await}
-import scala.concurrent.duration._
+import com.coreos.jetcd.Watch._
 import java.security.SecureRandom
+import scala.collection.JavaConverters._
+import scala.compat.java8.FutureConverters._
+import scala.concurrent.{blocking, Await, Future}
+import scala.concurrent.duration._
 import scala.util.Random
 
 object Main extends App {
@@ -18,24 +20,45 @@ object Main extends App {
   implicit val mat = ActorMaterializer()
   import sys.dispatcher
 
+  val leaseTtl = 5
+  val leaderKey = "/leader"
+  val keySeq = ByteSequence.fromString(leaderKey)
   val rand = new Random(new SecureRandom)
   val nodeId = rand.alphanumeric.take(5).mkString
-  println(s"node id: $nodeId")
+  val nodeIdSeq = ByteSequence.fromString(nodeId)
+  println(s"Watch $leaderKey key\nNode id: $nodeId")
 
-  val client = Client.builder().endpoints("http://127.0.0.1:2379").build()
+  def getWatcher(client: Client): Future[Watcher] =
+    Future(blocking(client.getWatchClient().watch(keySeq)))
+
+  def electionWatcher(w: Watcher): Future[Option[List[WatchEvent]]] =
+    Future(blocking {
+      val xs = w.listen().getEvents().asScala.toList
+      Some(xs)
+    })
+
+  def closeWatcher(w: Watcher): Future[Done] =
+    Future(blocking { w.close(); Done })
+
+  val client = Client.builder().endpoints("http://127.0.0.1:2379").lazyInitialization(true).build()
+  val kvClient = client.getKVClient()
+  val leaseClient = client.getLeaseClient()
+
   Source
-    .unfoldResourceAsync[List[WatchEvent], Watcher](
-      () => Future(blocking(client.getWatchClient().watch(ByteSequence.fromString("/leader")))),
-      { watcher => Future(blocking(Some(watcher.listen().getEvents().asScala.toList))) },
-      { watcher => Future(blocking { watcher.close(); Done })} )
+    .unfoldResourceAsync[List[WatchEvent], Watcher](() => getWatcher(client), electionWatcher, closeWatcher)
     .mapConcat(identity)
     .runWith(Sink.foreach { event =>
       val kv = event.getKeyValue()
-      println(
-        (event.getEventType(),
-         kv.getKey().toStringUtf8(),
-         kv.getValue().toStringUtf8()))
+      println((event.getEventType(), kv.getKey().toStringUtf8(), kv.getValue().toStringUtf8()))
     })
+
+  val keyCmp = new Cmp(keySeq, Cmp.Op.EQUAL, CmpTarget.version(0))
+
+  for {
+    grantRes <- leaseClient.grant(leaseTtl).toScala
+    option = PutOption.newBuilder().withLeaseId(grantRes.getID()).build()
+    res <- kvClient.txn().If(keyCmp).Then(Op.put(keySeq, nodeIdSeq, option)).commit().toScala
+  } yield println(s"tx: $res, success: ${res.isSucceeded}")
 
   Await.result(sys.whenTerminated, Duration.Inf)
 }
