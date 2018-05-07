fe762fda8ad715a1e563e4fdc9f4330ee04234ed
Timothy Klim <klimtimothy@gmail.com>
2018-05-07 15:00:54 +0300

    events

--- build.sbt
+++ build.sbt
@@ -4,6 +4,11 @@ scalaVersion := "2.12.6"
 
 val akkaVersion = "2.5.12"
 
+resolvers := Seq(
+  Resolver.jcenterRepo,
+  Resolver.sonatypeRepo("releases")
+)
+
 libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "com.coreos" % "jetcd-core" % "0.0.2",
@@ -14,3 +19,5 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-stream" % akkaVersion,
   "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0",
 )
+
+scalafmtOnCompile in ThisBuild := true
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,14 @@
 import akka.actor._
 import akka.Done
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.stream.scaladsl._
 import com.coreos.jetcd._
 import com.coreos.jetcd.data._
 import com.coreos.jetcd.op._
 import com.coreos.jetcd.options._
-import com.coreos.jetcd.watch._
+import com.coreos.jetcd.watch._, WatchEvent.EventType
 import com.coreos.jetcd.Watch._
+import com.coreos.jetcd.kv._
 import java.security.SecureRandom
 import scala.collection.JavaConverters._
 import scala.compat.java8.FutureConverters._
@@ -31,9 +32,24 @@ object Main extends App {
   def getWatcher(client: Client): Future[Watcher] =
     Future(blocking(client.getWatchClient().watch(keySeq)))
 
-  def electionWatcher(w: Watcher): Future[Option[List[WatchEvent]]] =
+  def electionWatcher(w: Watcher): Future[Option[List[EtcdEvent]]] =
     Future(blocking {
-      val xs = w.listen().getEvents().asScala.toList
+      val xs = w
+        .listen()
+        .getEvents()
+        .asScala
+        .toList
+        .map { evt =>
+          val kv = evt.getKeyValue()
+          if (kv.getKey().toStringUtf8() != leaderKey) None
+          else
+            evt.getEventType() match {
+              case EventType.PUT          => Some(EtcdEvent.Put(kv.getValue().toStringUtf8()))
+              case EventType.DELETE       => Some(EtcdEvent.Delete)
+              case EventType.UNRECOGNIZED => None
+            }
+        }
+        .flatten
       Some(xs)
     })
 
@@ -44,21 +60,49 @@ object Main extends App {
   val kvClient = client.getKVClient()
   val leaseClient = client.getLeaseClient()
 
-  Source
-    .unfoldResourceAsync[List[WatchEvent], Watcher](() => getWatcher(client), electionWatcher, closeWatcher)
+  val etcdEventsSource = Source
+    .unfoldResourceAsync[List[EtcdEvent], Watcher](() => getWatcher(client), electionWatcher, closeWatcher)
     .mapConcat(identity)
-    .runWith(Sink.foreach { event =>
-      val kv = event.getKeyValue()
-      println((event.getEventType(), kv.getKey().toStringUtf8(), kv.getValue().toStringUtf8()))
-    })
 
-  val keyCmp = new Cmp(keySeq, Cmp.Op.EQUAL, CmpTarget.version(0))
+  etcdEventsSource.runWith(Sink.foreach(println))
 
-  for {
-    grantRes <- leaseClient.grant(leaseTtl).toScala
-    option = PutOption.newBuilder().withLeaseId(grantRes.getID()).build()
-    res <- kvClient.txn().If(keyCmp).Then(Op.put(keySeq, nodeIdSeq, option)).commit().toScala
-  } yield println(s"tx: $res, success: ${res.isSucceeded}")
+  val eventsSource: Source[Event, _] = etcdEventsSource
+  // .collect {
+  //   case evt @ EtcdEvent.Delete => evt
+  // }
+    .map(Event.Etcd(_))
+    .prepend(Source.single[Event](Event.Init))
+
+  Source
+    .fromFuture(leaseClient.grant(leaseTtl).toScala)
+    .flatMapConcat { grant =>
+      val opt = PutOption.newBuilder().withLeaseId(grant.getID()).build()
+      val keyCmp = new Cmp(keySeq, Cmp.Op.EQUAL, CmpTarget.version(0))
+      Source
+        .tick(0.seconds, 1.second, Event.Tick)
+        .buffer(size = 1, OverflowStrategy.dropHead)
+        .merge(eventsSource)
+        .scanAsync(Option.empty[TxnResponse]) {
+          case (prevTxnOpt, evt: Event) =>
+            println("Trying to become a leader")
+            kvClient.txn().If(keyCmp).Then(Op.put(keySeq, nodeIdSeq, opt)).commit().toScala.map(Some(_))
+        }
+    }
+    .collect { case Some(res) => res }
+    .runWith(Sink.foreach(res => println(s"tx: $res, success: ${res.isSucceeded}")))
 
   Await.result(sys.whenTerminated, Duration.Inf)
+}
+
+sealed trait EtcdEvent
+object EtcdEvent {
+  case object Delete extends EtcdEvent
+  final case class Put(value: String) extends EtcdEvent
+}
+
+sealed trait Event
+object Event {
+  case object Init extends Event
+  case object Tick extends Event
+  final case class Etcd(evt: EtcdEvent) extends Event
 }
